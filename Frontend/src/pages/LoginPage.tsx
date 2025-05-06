6e8c9c4f38cd2d5bd76fab07dd4dc057a1a9621d
Camilo Gomez <camigomezbernal@gmail.com>
2025-05-06 15:12:47 -0500

    Organizamos Login

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import LoginForm from "../components/LoginForm";
-import Franco from "../assets/Franco.png";
-import Logo_casa_colaborador from "../assets/Casa_Colaborador.png";
-import Trabajadores from "../assets/Trabajadores.png";
-import Logo_Home from "../assets/Logo_home.png";
+import LoginForm from "../components/Form_Login/LoginForm";
+import Franco from "../assets/images/Franco.png";
+import Trabajadores from "../assets/images/Trabajadores.png";
+import Logo_Home from "../assets/logos/Logo_home.png";
 
 const LoginPage: React.FC = () => {
     const handleLogin = (email: string, idToken: string) => {
@@ -16,50 +15,44 @@ const LoginPage: React.FC = () => {
             <div className="absolute inset-0 z-0">
                 {/* IMAGENES */}
 
-                {/* CASA COLABORADOR */}
-                <img 
-                src={Logo_casa_colaborador}
-                alt="Logo Casa del Colaborador"
-                className="absolute top-[-90px] left-1/2 transform -translate-x-1/2 w-[600px] object-contain" 
-                />
                 {/* LOGO HOME */}
                 <img 
                 src={Logo_Home} 
                 alt="Logo Homecenter" 
-                className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[150px] object-contain"
+                className="absolute top-3 left-1/2 transform -translate-x-1/2 w-[150px] object-contain" 
                 />
                 {/* FRANCO */}
                 <img 
                 src={Franco} 
                 alt="Franco" 
-                className="absolute left-[-190px] top-1/2 transfotrm -translate-y-1/2 w-[750px] object-contain"
+                className="absolute left-[-140px] top-1/2 transfotrm -translate-y-1/2 w-[500px] object-contain"
                 />
                 {/* TRABAJADORES */}
                 <img 
                 src={Trabajadores} 
                 alt="Trabajadores" 
-                className="absolute right-0 top-1/2 transform -translate-y-1/2 w-[400px] object-contain"
+                className="absolute right-[0px] top-1/2 transform -translate-y-1/2 w-[375px] object-contain"
                 />
             </div>
 
             {/* CONTENEDOR DEL FORMULARIO */}
-            <div className="relative z-10 flex items-center justify-center min-h-screen px-4">
+            <div className="font-poppins relative z-10 flex items-center justify-center min-h-screen px-4">
                 {/* FORMULARIO */}
-                <div className="bg-[#4669AF] p-8 rounded-md text-center space-y-6 shadow-2xl w-full max-w-xl">
+                <div className="bg-[#4669AF] p-10 rounded-3xl text-center space-y-6 shadow-2xl transform transition duration-300 hover:scale-105 w-full max-w-lg">
                     <h2 className="text-xl font-bold text-white">
                     Bienvenido(a), aquí podrás gestionar tus diferentes procesos de Nómina
                     </h2>
 
                     <LoginForm onGoogleLogin={handleLogin} />
 
-                    <div className="space-y-2 text-white">
-                        <p className="cursor-pointer">
+                    <div className="space-y-3 text-white">
+                        <p className="cursor-pointer font-bold hover:text-gray-200 transition duration-300">
                             ¿Tienes dudas del portal? <br />
-                            <span className=" text-white text-sm">Resuélvelas aquí</span>
+                            <span className="font-normal text-white text-sm">Resuélvelas aquí</span>
                         </p>
-                        <p className="cursor-pointer">
+                        <p className="cursor-pointer font-bold hover:text-gray-200 transition duration-300">
                             ¿No logras ingresar? <br />
-                            <span className=" text-white text-sm">Crear caso con soporte SODI</span>
+                            <span className="font-normal text-white text-sm">Crear caso con soporte SODI</span>
                         </p>
                     </div>
                 </div>
--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DashboardEmpleado from '../pages/Dashboard/Dashboard_Empleado/DashboardEmpleado'
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import DashboardEmpleado from '../pages/Dashboard/Dashboard_Empleado/DashboardEmpleado'
 
-export const AppRoutes = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<DashboardEmpleado />}/>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+// export const AppRoutes = () => {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element={<DashboardEmpleado />}/>
+//             </Routes>
+//         </BrowserRouter>
+//     )
+// }
 
-export default AppRoutes
+// export default AppRoutes
