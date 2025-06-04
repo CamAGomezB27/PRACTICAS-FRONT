baf7e6897a61aa089a5fe94f8daf188341d76b98
Camilo Gomez <camigomezbernal@gmail.com>
2025-06-04 11:38:03 -0500

    Se genera index.cc en routes para mejor modularidad

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/Login/LoginPage';
-import DashAdmin from './pages/Dashboard/Admin/DashboardAdministrador';
-import DashJefe from './pages/Dashboard/Jefe/DashboardJefe'
-import DashNomina from './pages/Dashboard/Nomina/DashboardNomina';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes';
 
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard-administrador" element={<DashAdmin />} />
-        <Route path='/dashboard-jefe' element={<DashJefe/>} />
-        <Route path='/dashboard-nomina' element={<DashNomina/>} />
-      </Routes>
+      <AppRoutes />
     </Router>
   );
 };
--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -1,14 +1,18 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import DashboardEmpleado from '../pages/Dashboard/Dashboard_Empleado/DashboardEmpleado'
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/Login/LoginPage';
+import DashAdmin from '../pages/Dashboard/Admin/DashboardAdministrador';
+import DashJefe from '../pages/Dashboard/Jefe/DashboardJefe';
+import DashNomina from '../pages/Dashboard/Nomina/DashboardNomina';
 
-// export const AppRoutes = () => {
-//     return (
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route path="/" element={<DashboardEmpleado />}/>
-//             </Routes>
-//         </BrowserRouter>
-//     )
-// }
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/dashboard-administrador" element={<DashAdmin />} />
+      <Route path="/dashboard-jefe" element={<DashJefe />} />
+      <Route path="/dashboard-nomina" element={<DashNomina />} />
+    </Routes>
+  );
+};
 
-// export default AppRoutes
+export default AppRoutes;
