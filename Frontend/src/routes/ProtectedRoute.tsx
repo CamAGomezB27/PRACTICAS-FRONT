fb5b68e1755c5c98cd670b022ec4c43cb6e121c9
Camilo Gomez <camigomezbernal@gmail.com>
2025-06-09 15:31:21 -0500

    Se genera protección de rutas

---
+++ Frontend/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/useAuth';
+
+interface ProtectedRouteProps {
+  allowedRoles: string[];
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ allowedRoles }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/" replace />; //No login
+  }
+
+  let userRole = '';
+  if (user.esAdmin) userRole = 'admin';
+  else if (user.esNomina) userRole = 'nomina';
+  else if (user.esJefe) userRole = 'jefe';
+
+  if (!allowedRoles.includes(userRole)) {
+    return <Navigate to="/unauthorized" replace />; //Sin permisos de rol
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
--- Frontend/src/routes/index.tsx
+++ Frontend/src/routes/index.tsx
@@ -3,14 +3,24 @@ import LoginPage from '../pages/Login/LoginPage';
 import DashAdmin from '../pages/Dashboard/Admin/DashboardAdministrador';
 import DashJefe from '../pages/Dashboard/Jefe/DashboardJefe';
 import DashNomina from '../pages/Dashboard/Nomina/DashboardNomina';
+import ProtectedRoute from './ProtectedRoute';
+// import Unauthorized from '../pages/Unauthorized'; // Puedes crear una simple página
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/dashboard-administrador" element={<DashAdmin />} />
-      <Route path="/dashboard-jefe" element={<DashJefe />} />
-      <Route path="/dashboard-nomina" element={<DashNomina />} />
+      {/* RUTAS */}
+      <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
+        <Route path="/dashboard-administrador" element={<DashAdmin />} />
+      </Route>
+      <Route element={<ProtectedRoute allowedRoles={['nomina']} />}>
+        <Route path="/dashboard-nomina" element={<DashNomina />} />
+      </Route>
+      <Route element={<ProtectedRoute allowedRoles={['jefe']} />}>
+        <Route path="/dashboard-jefe" element={<DashJefe />} />
+      </Route>
+      {/* MÁS RUTAS */}
     </Routes>
   );
 };
