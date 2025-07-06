26a8c634578c709813985ba5833340b7891887dd
Camilo Gomez <camigomezbernal@gmail.com>
2025-07-05 22:06:15 -0500

    Se genera vista de formulario de desceunto

--- Frontend/src/components/Form_Solicitudes/FormIndivi.tsx
+++ Frontend/src/components/Form_Solicitudes/FormIndivi.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import FormularioAuxTransp from './FormAuxTransp';
-import FormularioOtroSiDef from './FromOtrSDef';
-import FormularioVacaciones from './FormVacaciones';
-import FormularioOtroSiTemporal from './FormOtroSiTemp';
+import FormularioDesc from './FormDescuentos';
 import FormularioHorasExtra from './FormHorasExt';
 import FormularioOtros from './FormOtros';
+import FormularioOtroSiTemporal from './FormOtroSiTemp';
+import FormularioVacaciones from './FormVacaciones';
+import FormularioOtroSiDef from './FromOtrSDef';
 
 const Individual: React.FC = () => {
   const { state } = useLocation();
@@ -15,6 +16,8 @@ const Individual: React.FC = () => {
     switch (titulo) {
       case 'Auxilio de transporte':
         return <FormularioAuxTransp />;
+      case 'Descuento':
+        return <FormularioDesc />;
       case 'Otro Si Definitivo':
         return <FormularioOtroSiDef />;
       case 'Vacaciones':
